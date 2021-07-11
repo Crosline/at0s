ea49e1e7115f0ccf04ec752e920f0c138a1bb53a
alperenaydas <b21726947@cs.hacettepe.edu.tr>
2021-07-11 16:07:53 +0300

    something happening

--- Assets/Scripts/Drawer.cs
+++ Assets/Scripts/Drawer.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 public class Drawer : MonoBehaviour {
+
+    public bool isOpen = false;
+
     void OnCollisionEnter(Collision collision) {
         collision.collider.transform.SetParent(transform);
     }

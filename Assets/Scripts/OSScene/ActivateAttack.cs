71578131477d15f82d884d08c0deb55d9a2b6ae5
Crosline <cagkancaglayanel@gmail.com>
2021-07-11 20:10:46 +0300

    snek exe

--- Assets/Scripts/OSScene/ActivateAttack.cs
+++ Assets/Scripts/OSScene/ActivateAttack.cs
@@ -12,6 +12,18 @@ public class ActivateAttack : MonoBehaviour {
         if (name.Contains("Hour")) {
             GetComponent<Timer>().IncreaseTimeOffset();
         }
+        else if (name.Contains("Controller")) {
+            Snake snake = transform.parent.parent.GetChild(2).GetComponent<Snake>();
+            if (name.Contains("bottom")) {
+                snake.UpdateDirection(1);
+            } else if (name.Contains("right")) {
+                snake.UpdateDirection(2);
+            } else if (name.Contains("left")) {
+                snake.UpdateDirection(3);
+            } else if (name.Contains("top")) {
+                snake.UpdateDirection(4);
+            }
+        }
     }
     /*
     private void OnTriggerEnter2D(Collider2D other) {
